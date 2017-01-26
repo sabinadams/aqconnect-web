cebed8d4c621b140de832db53eaf4d54da1c149e
M̰͒o̳̲̭͓̗͈̍͟n͐̄ͪͥͧk̉ <sabintheworld@gmail.com>
2017-01-26 02:15:28 -0800

    Notification stuff
    yeh

--- src/app/pages/navbar/nav.ts
+++ src/app/pages/navbar/nav.ts
@@ -14,8 +14,11 @@ export class NavComponent implements OnInit{
 	constructor(private _router: Router, private _authService: AuthService, private _serverStatus: ServerStatusService, private _notificationService: NotificationService){}
 	notification_check = false;
 	notifications:any;
-
+	start = 0;
 	ngOnInit(){
+		this.notification_check = false;
+		this.start = 0;
+		this.notifications = [];
 		this.user = JSON.parse(localStorage.getItem('user'));
 		this._serverStatus.getStats().subscribe((res) => {
 			this.servers = JSON.parse(res);
@@ -34,11 +37,38 @@ export class NavComponent implements OnInit{
 	}
 
 	handleNotifications(){
-		this._notificationService.checkNotifications().subscribe(res => {
-			if(res.status != 401 ){
-				this.notification_check = true;
-				this.notifications = res;
-			}
+		console.log(localStorage.getItem('user'));
+		if(localStorage.getItem('user')){
+			this._notificationService.checkNotifications().subscribe(res => {
+				if(res.status != 401 ){
+					this.notifications = res;
+					if(this.start + res.length < 100){
+						this.start += res.length;
+					}
+					this.notification_check = false;
+					for(let notification of this.notifications){
+						if(notification.read == 0) this.notification_check = true;
+					}
+
+				}
+			});
+		}
+		
+	}
+
+	clearAllNotifications(){
+		this._notificationService.clearAllNotifications().subscribe(res => {
+			console.log(res);
+			this.handleNotifications();
+			this.notification_check = false;
+		});
+		this.notifications = [];
+	}
+
+	readNotification(ID){
+		this._notificationService.readNotification(ID).subscribe(res => {
+			console.log(res);
+			this.handleNotifications();
 		});
 	}
 }
--- src/app/services/notification-service.ts
+++ src/app/services/notification-service.ts
@@ -7,14 +7,36 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class NotificationService {
   constructor(private _httpClient: HttpClient, private _http: Http) {}
-  user = JSON.parse(localStorage.getItem('user'));
+  
   //Likes and unlikes forum posts
   checkNotifications(){
-    let data = {'userID': this.user.Id, 'token': this.user.token};
+    let user = JSON.parse(localStorage.getItem('user'));
+    let data = {'userID': user.Id, 'token': user.token};
     return this._httpClient.post('http://aq.trycf.com/api/index.cfm/checknotifications/', {'data':data}) // ...using post request
     .map((res:Response) => {
         return res.json();
      }) // ...and calling .json() on the response to return data
     .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
   }
+
+  clearAllNotifications(){
+    let user = JSON.parse(localStorage.getItem('user'));
+    let data = {'userID': user.Id, 'token': user.token};
+    return this._httpClient.post('http://aq.trycf.com/api/index.cfm/clearallnotifications/', {'data':data}) // ...using post request
+    .map((res:Response) => {
+        return res.json();
+     }) // ...and calling .json() on the response to return data
+    .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
+  readNotification( ID ){
+    let user = JSON.parse(localStorage.getItem('user'));
+    let data = {'userID': user.Id, 'token': user.token, 'notificationID': ID};
+    return this._httpClient.post('http://aq.trycf.com/api/index.cfm/readnotification/', {'data':data}) // ...using post request
+    .map((res:Response) => {
+        return res.json();
+     }) // ...and calling .json() on the response to return data
+    .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+  }
+
 }
